d97a77ed06d6187f483282c2cac8332f59bbcae2
RatiboR1978 <klov29@mail.ru>
2017-10-02 10:18:53 +0700

    Прицепил к инпутам не много функционала

--- js/common.js
+++ js/common.js
@@ -1,5 +1,32 @@
-$(function () {
+(function() {
+    let contacts = document.querySelector('.user__contacts'),
+        divAll = document.querySelectorAll('.user__info-edit'),
+        inputAll = document.querySelectorAll('.user__input'),
+        pAll = document.querySelectorAll('.user__contacts-item > p');
 
+    contacts.addEventListener('click', function (event) {
+        for(let i = 0; i < pAll.length; i++) {
+            pAll[i].style.display = 'block';
+            divAll[i].style.display = 'none';
+            if(event.target === pAll[i]) {
+                pAll[i].style.display = 'none';
+                divAll[i].style.display = 'block';
+                inputAll[i].focus();
+                inputAll[i].addEventListener('blur', function () {
+                    console.log(inputAll[i].value);
+                    pAll[i].innerHTML = inputAll[i].value;
+                    pAll[i].style.display = 'block';
+                    divAll[i].style.display = 'none';
+                });
+                inputAll[i].addEventListener('keyup', function (e) {
+                    if(e.keyCode === 13) {
+                        pAll[i].innerHTML = inputAll[i].value;
+                        pAll[i].style.display = 'block';
+                        divAll[i].style.display = 'none';
+                    }
+                })
+            }
+        }
+    });
+}());
 
-
-});
